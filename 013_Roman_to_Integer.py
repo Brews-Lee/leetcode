7969105f4bb92f68ba9913edd606d6d1f74c32ad
BrewsLee <ondratep@gmail.com>
2024-01-18 16:35:04 +0100

    problems 9 and 13

---
+++ 013_Roman_to_Integer.py
@@ -0,0 +1,50 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        def turn_to_numbers(roman: str) -> list[int]:
+            nums = list()
+            for char in roman:
+                if char == "I":
+                    nums.append(1)
+                elif char == "V":
+                    nums.append(5)
+                elif char == "X":
+                    nums.append(10)
+                elif char == "L":
+                    nums.append(50)
+                elif char == "C":
+                    nums.append(100)
+                elif char == "D":
+                    nums.append(500)
+                elif char == "M":
+                    nums.append(1000)
+            return nums
+        nums = turn_to_numbers(s)
+
+        result = 0
+        i = 0
+        for num in nums:
+            if i >= len(nums):
+                break
+            if i == len(nums) - 1:
+                result += num
+                break
+            # Next number is the same
+            if nums[i] == nums[i + 1]:
+                if i != len(nums) - 2 and nums[i] == nums[i + 2]:
+                    result += 3 * num
+                    i += 3
+                    continue
+                else:
+                    result += 2 * num
+                    i += 2
+                    continue
+            # Next number is larger
+            elif nums[i] < nums[i + 1]:
+                result += nums[i + 1] - nums[i]
+            # Next number is smaller
+            else:
+                result += num
+            i += 1
+        return result
+
+print(Solution().romanToInt("III"))
