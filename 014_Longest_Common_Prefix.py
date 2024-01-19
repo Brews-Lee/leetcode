3bd7787cc7843acc297ffcd5932bd22cb577aa1d
BrewsLee <ondratep@gmail.com>
2024-01-19 12:34:07 +0100

    Problem 14 - finished

--- 014_Longest_Common_Prefix.py
+++ 014_Longest_Common_Prefix.py
@@ -1,10 +1,33 @@
+# https://leetcode.com/problems/longest-common-prefix/description/
+
 class Solution:
     def longestCommonPrefix(self, strs: list[str]) -> str:
+        prefix = ""
         def find_common(s1: str, s2: str) -> str:
-            for char in enumerate(s1)
+            common = ""
+            count = 0
+            if len(s1) > len(s2):
+                count = len(s2)
+            else:
+                count = len(s1)
 
-        ref = strs[0]
-
-
-
-print(Solution().longestCommonPrefix(["flower", "flow", "flash"]))
+            for i, char in enumerate(s1):
+                if i == count:
+                    break
+                if s1[i] == s2[i]:
+                    common += char
+                else:
+                    break
+            return common
+        if not len(strs) == 1:
+            prefix = find_common(strs[0], strs[1])
+        else:
+            prefix = strs[0]
+            return prefix
+        i = 2
+        for s in strs:
+            if i == len(strs):
+                break
+            prefix = find_common(prefix, strs[i])
+            i += 1
+        return prefix
